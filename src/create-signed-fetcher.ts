db190de009e27efc34cb6a4237756f52cc0f2d8b
zirkelc <chris.zirkel@gmail.com>
2024-01-15 09:34:03 +0100

    lint and format

--- src/create-signed-fetcher.ts
+++ src/create-signed-fetcher.ts
@@ -1,13 +1,13 @@
-import { Sha256 } from '@aws-crypto/sha256-js';
-import { defaultProvider } from '@aws-sdk/credential-provider-node';
-import { HttpRequest } from '@aws-sdk/protocol-http';
+import { Sha256 } from "@aws-crypto/sha256-js";
+import { defaultProvider } from "@aws-sdk/credential-provider-node";
+import { HttpRequest } from "@aws-sdk/protocol-http";
 // import fetch, { Headers } from 'cross-fetch';
 // import { Credentials, Provider, QueryParameterBag } from "@aws-sdk/types";
-import { parseQueryString } from '@aws-sdk/querystring-parser';
-import { SignatureV4 } from '@aws-sdk/signature-v4';
-import { getFetchFn } from './get-fetch';
-import { getHeaders } from './get-headers';
-import { CreateSignedFetcher, SignedFetcherOptions } from './types';
+import { parseQueryString } from "@aws-sdk/querystring-parser";
+import { SignatureV4 } from "@aws-sdk/signature-v4";
+import { getFetchFn } from "./get-fetch";
+import { getHeaders } from "./get-headers";
+import { CreateSignedFetcher, SignedFetcherOptions } from "./types";
 
 // TODO
 // export types
@@ -23,48 +23,56 @@ import { CreateSignedFetcher, SignedFetcherOptions } from './types';
  * @param init
  * @returns fetch
  */
-export const createSignedFetcher: CreateSignedFetcher = (opts: SignedFetcherOptions): typeof fetch => {
-  const service = opts.service; // TODO match service from URL
-  const region = opts.region || 'us-east-1'; // TODO match region from URL
-  const credentials = opts.credentials || defaultProvider();
+export const createSignedFetcher: CreateSignedFetcher = (
+	opts: SignedFetcherOptions,
+): typeof fetch => {
+	const service = opts.service; // TODO match service from URL
+	const region = opts.region || "us-east-1"; // TODO match region from URL
+	const credentials = opts.credentials || defaultProvider();
 
-  const fetchFn = getFetchFn(opts.fetch);
+	const fetchFn = getFetchFn(opts.fetch);
 
-  return async (input, init?) => {
-    const url = new URL(typeof input === 'string' ? input : input instanceof URL ? input.href : input.url);
+	return async (input, init?) => {
+		const url = new URL(
+			typeof input === "string"
+				? input
+				: input instanceof URL
+				  ? input.href
+				  : input.url,
+		);
 
-    const headers = getHeaders(init?.headers);
-    // host is required by AWS Signature V4: https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
-    headers.set('host', url.host);
+		const headers = getHeaders(init?.headers);
+		// host is required by AWS Signature V4: https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+		headers.set("host", url.host);
 
-    const request = new HttpRequest({
-      hostname: url.hostname,
-      path: url.pathname,
-      protocol: url.protocol,
-      port: url.port ? Number( url.port) : undefined,
-      username: url.username,
-      password: url.password,
-      method: init?.method.toUpperCase(), // method must be uppercase
-      body: init?.body,
-      query: Object.fromEntries(url.searchParams.entries()),
-      fragment: url.hash,
-      headers: Object.fromEntries(headers.entries()),
-    });
+		const request = new HttpRequest({
+			hostname: url.hostname,
+			path: url.pathname,
+			protocol: url.protocol,
+			port: url.port ? Number(url.port) : undefined,
+			username: url.username,
+			password: url.password,
+			method: init?.method.toUpperCase(), // method must be uppercase
+			body: init?.body,
+			query: Object.fromEntries(url.searchParams.entries()),
+			fragment: url.hash,
+			headers: Object.fromEntries(headers.entries()),
+		});
 
-    const signer = new SignatureV4({
-      credentials,
-      service,
-      region,
-      sha256: Sha256,
-    });
+		const signer = new SignatureV4({
+			credentials,
+			service,
+			region,
+			sha256: Sha256,
+		});
 
-    const signedRequest = await signer.sign(request);
+		const signedRequest = await signer.sign(request);
 
-    return fetchFn(input, {
-      ...init,
-      headers: signedRequest.headers,
-      body: signedRequest.body,
-      method: signedRequest.method,
-    });
-  };
+		return fetchFn(input, {
+			...init,
+			headers: signedRequest.headers,
+			body: signedRequest.body,
+			method: signedRequest.method,
+		});
+	};
 };
--- src/get-fetch.ts
+++ src/get-fetch.ts
@@ -1,15 +1,18 @@
 export const getFetchFn = (customFetchFn?: typeof fetch): typeof fetch => {
-  if (customFetchFn) {
-    return customFetchFn;
-  }
+	if (customFetchFn) {
+		return customFetchFn;
+	}
 
-  if (typeof window !== 'undefined' && typeof window.fetch === 'function') {
-    return window.fetch.bind(window);
-  }
+	if (typeof window !== "undefined" && typeof window.fetch === "function") {
+		return window.fetch.bind(window);
+	}
 
-  if (typeof globalThis !== 'undefined' && typeof globalThis.fetch === 'function') {
-    return globalThis.fetch.bind(globalThis);
-  }
+	if (
+		typeof globalThis !== "undefined" &&
+		typeof globalThis.fetch === "function"
+	) {
+		return globalThis.fetch.bind(globalThis);
+	}
 
-  throw new Error('No fetch implementation found');
+	throw new Error("No fetch implementation found");
 };
