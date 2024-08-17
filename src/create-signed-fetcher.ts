f882966eaf22092d9326ca6959b9cb6b8edcf1b0
zirkelc <chris.zirkel@gmail.com>
2024-08-17 11:11:30 +0200

    fix: support Request object as input

--- src/create-signed-fetcher.ts
+++ src/create-signed-fetcher.ts
@@ -4,13 +4,13 @@ import type { AwsCredentialIdentity, Provider } from "@aws-sdk/types";
 import { HttpRequest } from "@smithy/protocol-http";
 import { SignatureV4 } from "@smithy/signature-v4";
 import { getFetchFn } from "./get-fetch.js";
-import { getHeaders } from "./get-headers.js";
+import { parseRequest } from "./parse-request.js";
 
 export type SignedFetcherOptions = {
-	service: string;
-	region?: string;
-	credentials?: AwsCredentialIdentity | Provider<AwsCredentialIdentity>;
-	fetch?: typeof fetch;
+  service: string;
+  region?: string;
+  credentials?: AwsCredentialIdentity | Provider<AwsCredentialIdentity>;
+  fetch?: typeof fetch;
 };
 
 export type CreateSignedFetcher = (init: SignedFetcherOptions) => typeof fetch;
@@ -23,56 +23,47 @@ export type CreateSignedFetcher = (init: SignedFetcherOptions) => typeof fetch;
  * @param init
  * @returns fetch
  */
-export const createSignedFetcher: CreateSignedFetcher = (
-	opts: SignedFetcherOptions,
-): typeof fetch => {
-	const fetchFn = getFetchFn(opts.fetch);
+export const createSignedFetcher: CreateSignedFetcher = (opts: SignedFetcherOptions): typeof fetch => {
+  const fetchFn = getFetchFn(opts.fetch);
 
-	return async (input, init?) => {
-		const service = opts.service;
-		const region = opts.region || "us-east-1";
-		const credentials = opts.credentials || defaultProvider();
+  return async (input, init?) => {
+    const service = opts.service;
+    const region = opts.region || "us-east-1";
+    const credentials = opts.credentials || defaultProvider();
 
-		const url = new URL(
-			typeof input === "string"
-				? input
-				: input instanceof URL
-					? input.href
-					: input.url,
-		);
+    const { url, method, body, headers } = parseRequest(input, init);
 
-		const headers = getHeaders(init?.headers);
-		// host is required by AWS Signature V4: https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
-		headers.set("host", url.host);
+    // host is required by AWS Signature V4: https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+    headers["host"] = url.host;
 
-		const request = new HttpRequest({
-			hostname: url.hostname,
-			path: url.pathname,
-			protocol: url.protocol,
-			port: url.port ? Number(url.port) : undefined,
-			username: url.username,
-			password: url.password,
-			method: init?.method?.toUpperCase() ?? "GET", // method must be uppercase
-			body: init?.body,
-			query: Object.fromEntries(url.searchParams.entries()),
-			fragment: url.hash,
-			headers: Object.fromEntries(headers.entries()),
-		});
+    const request = new HttpRequest({
+      method,
+      body,
+      headers,
+      hostname: url.hostname,
+      path: url.pathname,
+      protocol: url.protocol,
+      port: url.port ? Number(url.port) : undefined,
+      username: url.username,
+      password: url.password,
+      fragment: url.hash,
+      query: Object.fromEntries(url.searchParams.entries()),
+    });
 
-		const signer = new SignatureV4({
-			credentials,
-			service,
-			region,
-			sha256: Sha256,
-		});
+    const signer = new SignatureV4({
+      credentials,
+      service,
+      region,
+      sha256: Sha256,
+    });
 
-		const signedRequest = await signer.sign(request);
+    const signedRequest = await signer.sign(request);
 
-		return fetchFn(url, {
-			...init,
-			headers: signedRequest.headers,
-			body: signedRequest.body,
-			method: signedRequest.method,
-		});
-	};
+    return fetchFn(url, {
+      ...init,
+      headers: signedRequest.headers,
+      body: signedRequest.body,
+      method: signedRequest.method,
+    });
+  };
 };
