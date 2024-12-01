44d19e270783e8566c2f39eebc1eb83e1b8af3ad
zirkelc <chris.zirkel@gmail.com>
2024-12-01 09:19:01 +0100

    test: snapshots

--- src/__tests__/create-signed-fetcher.test.ts
+++ src/__tests__/create-signed-fetcher.test.ts
@@ -1,17 +1,18 @@
+import type { AwsCredentialIdentity } from "@aws-sdk/types";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { createSignedFetcher } from "../create-signed-fetcher.js";
 
-const urls = [
-  // "http://example.com",
-  // "http://example.com/foo",
-  // "http://example.com/foo?bar=baz",
-  "http://example.com/foo?bar=baz#qux",
-];
 const fetchMock = vi.fn(fetch);
 let signedFetch: typeof fetch;
 
 vi.useFakeTimers();
 vi.setSystemTime("2024-01-01T00:00:00.000Z");
+
+const credentials: AwsCredentialIdentity = {
+  accessKeyId: "dummyAccessKeyId",
+  secretAccessKey: "dummySecretAccessKey",
+  sessionToken: "dummySessionToken",
+};
 
 beforeEach(() => {
   vi.resetAllMocks();
@@ -19,14 +20,12 @@ beforeEach(() => {
   signedFetch = createSignedFetcher({
     service: "dummyService",
     region: "dummyRegion",
-    credentials: {
-      accessKeyId: "dummyAccessKeyId",
-      secretAccessKey: "dummySecretAccessKey",
-      sessionToken: "dummySessionToken",
-    },
+    credentials,
     fetch: fetchMock,
   });
 });
+
+const url = "http://example.com/foo?bar=baz#qux";
 
 const headersSigned = expect.objectContaining({
   host: "example.com",
@@ -48,62 +47,80 @@ const headersUnsignedPayload = expect.objectContaining({
   ),
 });
 
-// TODO use signed authorization header for all tests
-
 describe("createSignedFetcher", () => {
-  describe.each(urls)("URL: %s", (url) => {
-    describe.each([undefined, "GET", "POST"])("Method: %s", (method) => {
-      const expectedMethod = method ?? "GET";
+  describe("GET", () => {
+    const expectedMethod = "GET";
 
-      it("should fetch with string", async () => {
-        await signedFetch(url, method ? { method } : undefined);
+    it("should fetch with string", async () => {
+      await signedFetch(url);
 
-        expect(fetchMock).toHaveBeenCalled();
-        const [fetchUrl, fetchInit] = fetchMock.mock.calls[0];
+      expect(fetchMock).toHaveBeenCalled();
+      const [fetchUrl, fetchInit] = fetchMock.mock.calls[0];
 
-        expect(fetchUrl).toEqual(new URL(url));
-        expect(fetchInit?.method).toEqual(expectedMethod);
-        expect(fetchInit?.body).toEqual(undefined);
-        expect(fetchInit?.headers).toEqual(headersSigned);
-      });
+      expect(fetchUrl).toEqual(new URL(url));
+      expect(fetchInit?.method).toEqual(expectedMethod);
+      expect(fetchInit?.body).toEqual(undefined);
+      expect(fetchInit?.headers).toEqual(headersSigned);
 
-      it("should fetch with URL", async () => {
-        await signedFetch(new URL(url), method ? { method } : undefined);
-
-        expect(fetchMock).toHaveBeenCalled();
-        const [fetchUrl, fetchInit] = fetchMock.mock.calls[0];
-
-        expect(fetchUrl).toEqual(new URL(url));
-        expect(fetchInit?.method).toEqual(expectedMethod);
-        expect(fetchInit?.body).toEqual(undefined);
-        expect(fetchInit?.headers).toEqual(headersSigned);
-      });
-
-      it("should fetch with Request", async () => {
-        await signedFetch(new Request(url, method ? { method } : undefined));
-
-        expect(fetchMock).toHaveBeenCalled();
-        const [fetchUrl, fetchInit] = fetchMock.mock.calls[0];
-
-        expect(fetchUrl).toEqual(new URL(url));
-        expect(fetchInit?.method).toEqual(expectedMethod);
-        expect(fetchInit?.body).toEqual(null);
-        expect(fetchInit?.headers).toEqual(headersSigned);
-      });
-
-      it("should fetch with Request and options", async () => {
-        await signedFetch(new Request(url), method ? { method } : undefined);
-
-        expect(fetchMock).toHaveBeenCalled();
-        const [fetchUrl, fetchInit] = fetchMock.mock.calls[0];
-
-        expect(fetchUrl).toEqual(new URL(url));
-        expect(fetchInit?.method).toEqual(expectedMethod);
-        expect(fetchInit?.body).toEqual(null);
-        expect(fetchInit?.headers).toEqual(headersSigned);
-      });
+      const authorization = fetchInit?.headers?.["authorization"];
+      expect(authorization).toMatchInlineSnapshot(
+        `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=87967143809950c291d1539ed6a88fa6eb19cddbacfdae416db867d73e8fc08b"`,
+      );
     });
 
+    it("should fetch with URL", async () => {
+      await signedFetch(new URL(url));
+
+      expect(fetchMock).toHaveBeenCalled();
+      const [fetchUrl, fetchInit] = fetchMock.mock.calls[0];
+
+      expect(fetchUrl).toEqual(new URL(url));
+      expect(fetchInit?.method).toEqual(expectedMethod);
+      expect(fetchInit?.body).toEqual(undefined);
+      expect(fetchInit?.headers).toEqual(headersSigned);
+
+      const authorization = fetchInit?.headers?.["authorization"];
+      expect(authorization).toMatchInlineSnapshot(
+        `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=87967143809950c291d1539ed6a88fa6eb19cddbacfdae416db867d73e8fc08b"`,
+      );
+    });
+
+    it("should fetch with Request", async () => {
+      await signedFetch(new Request(url));
+
+      expect(fetchMock).toHaveBeenCalled();
+      const [fetchUrl, fetchInit] = fetchMock.mock.calls[0];
+
+      expect(fetchUrl).toEqual(new URL(url));
+      expect(fetchInit?.method).toEqual(expectedMethod);
+      expect(fetchInit?.body).toEqual(null);
+      expect(fetchInit?.headers).toEqual(headersSigned);
+
+      const authorization = fetchInit?.headers?.["authorization"];
+      expect(authorization).toMatchInlineSnapshot(
+        `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=87967143809950c291d1539ed6a88fa6eb19cddbacfdae416db867d73e8fc08b"`,
+      );
+    });
+
+    it("should fetch with Request and options", async () => {
+      await signedFetch(new Request(url));
+
+      expect(fetchMock).toHaveBeenCalled();
+      const [fetchUrl, fetchInit] = fetchMock.mock.calls[0];
+
+      expect(fetchUrl).toEqual(new URL(url));
+      expect(fetchInit?.method).toEqual(expectedMethod);
+      expect(fetchInit?.body).toEqual(null);
+      expect(fetchInit?.headers).toEqual(headersSigned);
+
+      const authorization = fetchInit?.headers?.["authorization"];
+      expect(authorization).toMatchInlineSnapshot(
+        `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=87967143809950c291d1539ed6a88fa6eb19cddbacfdae416db867d73e8fc08b"`,
+      );
+    });
+  });
+
+  describe("POST", () => {
     describe("Body: undefined", () => {
       const body = undefined;
 
@@ -117,6 +134,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersSigned);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=c11bdcfcc19e3e5f3f3afdd81a8bf9456650ed6a94803142a92cd432fdacc714"`,
+        );
       });
 
       it("should fetch with URL", async () => {
@@ -129,6 +151,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersSigned);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=c11bdcfcc19e3e5f3f3afdd81a8bf9456650ed6a94803142a92cd432fdacc714"`,
+        );
       });
 
       it("should fetch with Request", async () => {
@@ -141,6 +168,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(null);
         expect(fetchInit?.headers).toEqual(headersSigned);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=c11bdcfcc19e3e5f3f3afdd81a8bf9456650ed6a94803142a92cd432fdacc714"`,
+        );
       });
     });
 
@@ -157,6 +189,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersSigned);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=1c6004639bd60c69bfba7f46231d4f25423ae92a8a2746f489bed437273693dc"`,
+        );
       });
 
       it("should fetch with URL", async () => {
@@ -169,6 +206,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersSigned);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=1c6004639bd60c69bfba7f46231d4f25423ae92a8a2746f489bed437273693dc"`,
+        );
       });
 
       it("should fetch with Request", async () => {
@@ -181,6 +223,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(expect.any(ReadableStream));
         expect(fetchInit?.headers).toEqual(headersUnsignedPayload);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=30e1c46ef773fb1a3f82a537e15384d9601aebf38190e9ff03bdb2ff8a8c504f"`,
+        );
       });
     });
 
@@ -197,6 +244,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersUnsignedPayload);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=8edb7328e32484357649bb350b9c9d8b95d18c041fc7098ace02a3ab574afc8d"`,
+        );
       });
 
       it("should fetch with URL", async () => {
@@ -209,6 +261,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersUnsignedPayload);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=8edb7328e32484357649bb350b9c9d8b95d18c041fc7098ace02a3ab574afc8d"`,
+        );
       });
 
       it("should fetch with Request", async () => {
@@ -221,6 +278,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(expect.any(ReadableStream));
         expect(fetchInit?.headers).toEqual(headersUnsignedPayload);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=783834f3cc782c06f67ca1e74af2defb2cdf7b0e3690e887f5ca6bdee72003e1"`,
+        );
       });
     });
 
@@ -238,6 +300,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersUnsignedPayload);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=8edb7328e32484357649bb350b9c9d8b95d18c041fc7098ace02a3ab574afc8d"`,
+        );
       });
 
       it("should fetch with URL", async () => {
@@ -250,10 +317,29 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersUnsignedPayload);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=8edb7328e32484357649bb350b9c9d8b95d18c041fc7098ace02a3ab574afc8d"`,
+        );
       });
 
       it("should fetch with Request", async () => {
-        await signedFetch(new Request(url, { method: "POST", body }));
+        // Copied from: https://github.com/nodejs/undici/blob/fb3138d61d652866c54ff78eb182c40fe22f1c5d/test/fetch/formdata.js#L373-L388
+        await signedFetch(
+          new Request(url, {
+            method: "POST",
+            headers: {
+              "Content-Type": "multipart/form-data; boundary=----formdata-undici-0.6204674738279623",
+            },
+            body:
+              "------formdata-undici-0.6204674738279623\r\n" +
+              'Content-Disposition: form-data; name="fiŝo"\r\n' +
+              "\r\n" +
+              "value1\r\n" +
+              "------formdata-undici-0.6204674738279623--\r\n",
+          }),
+        );
 
         expect(fetchMock).toHaveBeenCalled();
         const [fetchUrl, fetchInit] = fetchMock.mock.calls[0];
@@ -262,6 +348,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(expect.any(ReadableStream));
         expect(fetchInit?.headers).toEqual(headersUnsignedPayload);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=f386b9865153ea2c54b5ece7cff7a3885419a25733e802dea5084b993c1e47d3"`,
+        );
       });
     });
 
@@ -278,6 +369,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersUnsignedPayload);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=8edb7328e32484357649bb350b9c9d8b95d18c041fc7098ace02a3ab574afc8d"`,
+        );
       });
 
       it("should fetch with URL", async () => {
@@ -290,6 +386,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersUnsignedPayload);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=8edb7328e32484357649bb350b9c9d8b95d18c041fc7098ace02a3ab574afc8d"`,
+        );
       });
 
       it("should fetch with Request", async () => {
@@ -302,6 +403,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(expect.any(ReadableStream));
         expect(fetchInit?.headers).toEqual(headersUnsignedPayload);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=8edb7328e32484357649bb350b9c9d8b95d18c041fc7098ace02a3ab574afc8d"`,
+        );
       });
     });
 
@@ -322,6 +428,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersSigned);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=f8f04362ccf0b2582c6e4fd9597578d1fcbcd831298cce0adb56adb451f4151d"`,
+        );
       });
 
       it("should fetch with URL", async () => {
@@ -338,6 +449,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(body);
         expect(fetchInit?.headers).toEqual(headersSigned);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=f8f04362ccf0b2582c6e4fd9597578d1fcbcd831298cce0adb56adb451f4151d"`,
+        );
       });
 
       it("should fetch with Request", async () => {
@@ -354,6 +470,11 @@ describe("createSignedFetcher", () => {
         expect(fetchInit?.method).toEqual("POST");
         expect(fetchInit?.body).toEqual(expect.any(ReadableStream));
         expect(fetchInit?.headers).toEqual(headersUnsignedPayload);
+
+        const authorization = fetchInit?.headers?.["authorization"];
+        expect(authorization).toMatchInlineSnapshot(
+          `"AWS4-HMAC-SHA256 Credential=dummyAccessKeyId/20240101/dummyRegion/dummyService/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=8edb7328e32484357649bb350b9c9d8b95d18c041fc7098ace02a3ab574afc8d"`,
+        );
       });
     });
   });
