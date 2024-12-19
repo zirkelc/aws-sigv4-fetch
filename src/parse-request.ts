360f33cc5e5e9d80e229ecdb995f64d9c055360d
zirkelc <chris.zirkel@gmail.com>
2024-12-19 18:00:04 +0100

    fix: remove old code

--- src/parse-request.ts
+++ src/parse-request.ts
@@ -57,80 +57,37 @@ export const copyHeaders = (headers?: HeadersInit): Record<string, string> => {
  * Init is an optional `RequestInit` object. If provided, it will override the values in the `Request` object.
  */
 export const parseRequest = async (input: string | Request | URL, init?: RequestInit): Promise<ParsedRequest> => {
-  let finalRequest: Request;
+  let request: Request;
 
+  /**
+   * Input can be a `string`, `URL`, or `Request` object
+   * `RequestInfo` is a union of `Request` and `string`
+   */
   if (typeof input === "string" || input instanceof URL) {
-    // If input is a string or a URL, first create a request from it
-    // If input is a Request, we directly construct from it.
     const url = input instanceof URL ? input.href : input;
-    finalRequest = new Request(url, init);
+    request = new Request(url, init);
   } else {
-    // input is a Request, create a new Request that applies init overrides
-    finalRequest = new Request(input, init);
+    request = new Request(input, init);
   }
 
-  // Extract properties from finalRequest
-  const method = finalRequest.method.toUpperCase();
-  const url = new URL(finalRequest.url);
-  const headers = copyHeaders(finalRequest.headers);
-
-  // Reading the body: If the request has a body, clone and read it.
-  // This example uses arrayBuffer, but you could use .text(), .json(), etc.
-  let body: BodyInit | null | undefined = undefined;
-  if (finalRequest.body) {
-    // Clone and read the body
-    const clonedRequest = finalRequest.clone();
-    body = await clonedRequest.arrayBuffer();
-  }
+  const method = request.method.toUpperCase();
+  const url = new URL(request.url);
+  const headers = copyHeaders(request.headers);
+  const body = request.body ? await request.clone().arrayBuffer() : undefined;
 
   return {
     method,
     headers,
     url,
     body,
-
-    credentials: finalRequest.credentials,
-    cache: finalRequest.cache,
-    mode: finalRequest.mode,
-    redirect: finalRequest.redirect,
-    referrer: finalRequest.referrer,
-    referrerPolicy: finalRequest.referrerPolicy,
-    integrity: finalRequest.integrity,
-    signal: finalRequest.signal,
-    duplex: finalRequest.duplex,
+    credentials: request.credentials,
+    cache: request.cache,
+    mode: request.mode,
+    redirect: request.redirect,
+    referrer: request.referrer,
+    referrerPolicy: request.referrerPolicy,
+    integrity: request.integrity,
+    signal: request.signal,
+    duplex: request.duplex,
   };
-
-  // let url = "";
-  // let method = "GET";
-  // let headers: HeadersInit | undefined = undefined;
-  // let body: BodyInit | null | undefined = undefined;
-
-  // /**
-  //  * Input can be a `string`, `URL`, or `Request` object
-  //  * `RequestInfo` is a union of `Request` and `string`
-  //  */
-  // if (typeof input === "string") {
-  //   url = input;
-  //   method = init?.method ?? "GET";
-  //   headers = init?.headers;
-  //   body = init?.body;
-  // } else if (input instanceof URL) {
-  //   url = input.href;
-  //   method = init?.method ?? "GET";
-  //   headers = init?.headers;
-  //   body = init?.body;
-  // } else if (isRequest(input)) {
-  //   url = input.url;
-  //   method = init?.method ?? input.method;
-  //   headers = init?.headers ?? input.headers;
-  //   body = init?.body ?? input.body ? await input.clone().arrayBuffer() : undefined;
-  // }
-
-  // return {
-  //   ...init,
-  //   method: method.toUpperCase(), // method must be uppercase
-  //   headers: copyHeaders(headers), // must be a plain object
-  //   url: new URL(url),
-  //   body,
-  // };
 };
--- src/sign-request.ts
+++ src/sign-request.ts
@@ -22,35 +22,32 @@ export async function signRequest(
     | [input: string | Request | URL, options: SignRequestOptions]
     | [input: string | Request | URL, init: RequestInit, options: SignRequestOptions]
 ): Promise<Request> {
-  let request: string | Request | URL;
+  let input: string | Request | URL;
   let init: RequestInit | undefined;
   let options: SignRequestOptions;
 
   if (args.length === 2) {
-    request = args[0];
+    input = args[0];
     options = args[1];
   } else {
-    request = args[0];
+    input = args[0];
     init = args[1];
     options = args[2];
   }
 
-  const { url, method, body, headers, ...requestInit } = await parseRequest(request, init);
-
-  // const parsedRequest = await parseRequest(request, init);
-  // const url = new URL(parsedRequest.url);
+  const { url, ...request } = await parseRequest(input, init);
 
   // host is required by AWS Signature V4: https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
-  headers["host"] = url.host;
+  request.headers["host"] = url.host;
 
   const service = options.service;
   const region = options.region || "us-east-1";
   const credentials = options.credentials || defaultProvider();
 
   const httpRequest = new HttpRequest({
-    method,
-    body,
-    headers,
+    method: request.method,
+    body: request.body,
+    headers: request.headers,
     hostname: url.hostname,
     path: url.pathname,
     protocol: url.protocol,
@@ -70,13 +67,5 @@ export async function signRequest(
 
   const { headers: signedHeaders } = await signer.sign(httpRequest);
 
-  // Copy only the signed headers, because the body may be modified by the signer
-  // parsedRequest.headers = signedHttpRequest.headers;
-  // for (const [key, value] of Object.entries(signedHttpRequest.headers)) {
-  //   parsedRequest.headers.set(key, value);
-  // }
-
-  const signedRequestInit = { ...requestInit, method, body, headers: signedHeaders };
-
-  return new Request(url, signedRequestInit);
+  return new Request(url, { ...request, headers: signedHeaders });
 }
